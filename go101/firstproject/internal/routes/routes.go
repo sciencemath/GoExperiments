f56e586be3773b73e658af04b440490a21321ae1
sciencemath <matrixmachinelearning@gmail.com>
2025-06-15 10:43:24 -0400

    finishing token implementation

--- go101/firstproject/internal/routes/routes.go
+++ go101/firstproject/internal/routes/routes.go
@@ -13,8 +13,10 @@ func SetupRoutes(app *app.Application) *chi.Mux {
 
 	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
 	r.Put("/workouts/{id}", app.WorkoutHandler.HandleUpdateWorkoutByID)
-
 	r.Delete("/workouts/{id}", app.WorkoutHandler.HandleDeleteWorkoutByID)
+
+	r.Post("/users", app.UserHandler.HandleRegisterUser)
+	r.Post("/tokens/authentication", app.TokenHandler.HandleCreateToken)
 
 	return r
 }
--- go101/firstproject/internal/store/user_store.go
+++ go101/firstproject/internal/store/user_store.go
@@ -2,14 +2,41 @@ package store
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 
+	"golang.org/x/crypto/bcrypt"
 	_ "golang.org/x/crypto/bcrypt"
 )
 
 type password struct {
 	plainText *string
 	hash      []byte
+}
+
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+
+	p.plainText = &plaintextPassword
+	p.hash = hash
+	return nil
+}
+
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
 }
 
 type User struct {
@@ -48,6 +75,63 @@ func (s *PostgresUserStore) CreateUser(user *User) error {
 	err := s.db.QueryRow(query, user.Username, user.Email, user.PasswordHash.hash, user.Bio).Scan(&user.ID, &user.CreatedAt, &user.UpdatedAt)
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func (s *PostgresUserStore) GetUserByUsername(username string) (*User, error) {
+	user := &User{
+		PasswordHash: password{},
+	}
+
+	query := `
+	SELECT id, username, email, password_hash, bio, created_at, updated_at
+	FROM users
+	WHERE username = $1
+	`
+
+	err := s.db.QueryRow(query, username).Scan(
+		&user.ID,
+		&user.Username,
+		&user.Email,
+		&user.PasswordHash.hash,
+		&user.Bio,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (s *PostgresUserStore) UpdateUser(user *User) error {
+	query := `
+	UPDATE users
+	SET username = $1, email = $2, bio = $3, updated_at = CURRENT_TIMESTAMP
+	WHERE id = $4
+	RETURNING updated_at
+	`
+
+	result, err := s.db.Exec(query, user.Username, user.Email, user.Bio, user.ID)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return sql.ErrNoRows
 	}
 
 	return nil
